925b5a34472bf6e1491b12341acec614a2cbf8a2
Marcio Silva <marcionps@proton.me>
2022-09-11 13:57:50 -0300

    added left sidebar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def svg(name)
+    file_path = "#{Rails.root}/app/assets/images/#{name}"
+    if File.exists?(file_path)
+      File.open(file_path, "rb") do |file|
+        raw file.read
+      end
+    else
+      '(not found)'
+    end
+  end
 end
