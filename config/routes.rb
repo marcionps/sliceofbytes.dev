c2334b31682148c728a743e1102053a2468c7cc6
Marcio Silva <marcionps@proton.me>
2022-09-15 16:56:34 -0300

    fixed misc links and added about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :posts
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "about", to: "pages#about"
+  resources :posts, except: [ :index ]
 
   # Defines the root path route ("/")
   root "posts#index"
