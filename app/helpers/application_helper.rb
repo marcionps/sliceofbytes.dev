3ad9e598165fd78545e847292e973d322692ad53
Marcio Silva <marcionps@proton.me>
2022-09-15 16:58:40 -0300

    added date formatter and post reading time

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,8 @@ module ApplicationHelper
       '(not found)'
     end
   end
+
+  def formatted_date(date)
+    date.strftime("%b %d, %Y")
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true, allow_blank: false
+
+    enum :status, { draft: 0, published: 1, archived: 2, trashed: 3 }, suffix: true
+    enum :visibility, { public: 0, private: 1 }, suffix: true
+
+    def reading_time
+        words_per_minute = 180
+
+        words = self.content.split.size;
+        minutes = ( words / words_per_minute ).floor
+        minutes_label = minutes === 1 ? " minute" : " minutes"
+        minutes > 0 ? "#{minutes} #{minutes_label} read" : "less than 1 minute read"
+    end
 end
