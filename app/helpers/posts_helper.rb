588c7d4c027f78d83e17f8c8f8eba9c9ba86a1e2
Marcio Silva <marcionps@proton.me>
2022-09-18 21:15:28 -0300

    added reading time

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,10 @@
 module PostsHelper
+    def reading_time(content)
+        words_per_minute = 180
+
+        words = content.split.size;
+        minutes = ( words / words_per_minute ).floor
+        minutes_label = minutes === 1 ? " minute" : " minutes"
+        minutes > 0 ? "#{minutes} #{minutes_label} read" : "less than 1 minute read"
+    end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -9,13 +9,4 @@ class Post < ApplicationRecord
 
     enum :status, { draft: 0, published: 1, archived: 2, trashed: 3 }, suffix: true
     enum :visibility, { public: 0, private: 1 }, suffix: true
-
-    def reading_time
-#        words_per_minute = 180
-#
-#        words = self.content.split.size;
-#        minutes = ( words / words_per_minute ).floor
-#        minutes_label = minutes === 1 ? " minute" : " minutes"
-#        minutes > 0 ? "#{minutes} #{minutes_label} read" : "less than 1 minute read"
-    end
 end
